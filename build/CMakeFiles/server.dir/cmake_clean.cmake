37d6378e536fb27606d1928dd53509056fbfafaa
huyuelin <57228384+huyuelin@users.noreply.github.com>
2023-05-04 20:38:24 +0800

    Add files via upload

---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/newQUIC.cpp.o"
+  "CMakeFiles/server.dir/server.cpp.o"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
